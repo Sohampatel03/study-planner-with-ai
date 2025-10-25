fb30d2bec3108c802d80bf0e42a26d461b8a90f7
sohampatel03 <soham171203@gmail.com>
2025-10-25 16:11:30 +0530

    change for deployment

--- frontend/src/pages/Register.js
+++ frontend/src/pages/Register.js
@@ -11,7 +11,7 @@ const Register = () => {
   const handleRegister = async (e) => {
     e.preventDefault();
     try {
-      const response = await fetch("http://localhost:5000/register", {
+      const response = await fetch("https://study-planner-with-ai-1.onrender.com/register", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
